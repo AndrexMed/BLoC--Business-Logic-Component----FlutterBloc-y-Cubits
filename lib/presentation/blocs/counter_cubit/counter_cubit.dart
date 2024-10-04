609b9392bb476f1ffcd9851a63c6009ee65ab72b
GiovanniAndrex <giovanniandrex@gmail.com>
2024-10-04 16:13:02 -0500

    Counter Cubit Gestor de estado

---
+++ lib/presentation/blocs/counter_cubit/counter_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(const CounterState(counterValue: 5));
+
+  void increment(int value) => emit(state.copyWith(
+        counterValue: state.counterValue + value,
+        transactionCount: state.transactionCount + 1,
+      ));
+
+  void reset() => emit(state.copyWith(
+        counterValue: 0,
+        transactionCount: state.transactionCount + 1,
+      ));
+}
